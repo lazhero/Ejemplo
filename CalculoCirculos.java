06d56381f706e59172e2337b9f40c660bc23813f
Andrey <luisandreyzuniga@gmail.com>
2020-02-03 12:23:30 -0600

    PrimeraVersion

---
+++ CalculoCirculos.java
@@ -0,0 +1,12 @@
+class CalculoCirculos{
+	public static void main(String[] args){
+
+		int r=5;
+		double pii=3.14,area,perimetro;
+		perimetro=2*pii*r;
+		area=pii*r*r;
+		System.out.println("El area es: "+area);
+		System.out.println("El perimetro es:" +perimetro);	
+	}
+
+}
